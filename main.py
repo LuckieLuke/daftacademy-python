c1d3f0606183adbc7373912b248a1a54da439bd3
LuckieLuke <luk19984@gmail.com>
2021-04-19 18:02:06 +0200

    first homework

--- main.py
+++ main.py
@@ -1,7 +1,63 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from hashlib import sha512
+from pydantic import BaseModel
+from datetime import datetime, timedelta
 
 app = FastAPI()
+app.users = []
+
+
+class RegisterUser(BaseModel):
+    name: str = ''
+    surname: str = ''
+
 
 @app.get('/')
 def root():
-    return {'message': 'Hello World!'}
+    return {'message': 'Hello world!'}
+
+
+@app.get('/method')
+@app.post('/method')
+@app.delete('/method')
+@app.put('/method')
+@app.options('/method')
+def get_method(request: Request):
+    code = 200 if request.method != 'POST' else 201
+    return JSONResponse(content={'method': request.method}, status_code=code)
+
+
+@app.get('/auth')
+async def auth(password: str = '', password_hash: str = ''):
+    code = 204 if sha512(
+        password.encode()).hexdigest() == password_hash else 401
+    return JSONResponse(status_code=code)
+
+
+@app.post('/register')
+async def register(user: RegisterUser):
+    id = len(app.users) + 1
+    register_date = datetime.today()
+    print(register_date)
+    diff = len(user.name) + len(user.surname)
+    vacc_date = register_date + timedelta(days=diff)
+    register_date = register_date.strftime('%Y-%m-%d')
+    vacc_date = vacc_date.strftime('%Y-%m-%d')
+    app.users.append({
+        'id': id,
+        'name': user.name,
+        'surname': user.surname,
+        'register_date': register_date,
+        'vaccination_date': vacc_date
+    })
+    return JSONResponse(content=app.users[id-1], status_code=201)
+
+
+@app.get('/patient/{id}')
+def patient(id: int):
+    if id < 1:
+        return JSONResponse(status_code=400)
+    if id > len(app.users):
+        return JSONResponse(status_code=404)
+    return JSONResponse(content=app.users[id-1], status_code=200)
--- tests.py
+++ tests.py
@@ -1,11 +1,78 @@
 from fastapi.testclient import TestClient
+from pydantic import BaseModel
+from datetime import datetime, timedelta
 
 from main import app
 
 client = TestClient(app)
 
 
+class RegisterUser(BaseModel):
+    name: str = ''
+    surname: str = ''
+
+
 def test_read_main():
     response = client.get('/')
     assert response.status_code == 200
-    assert response.json() == {'message': 'Hello World!'}
+    assert response.json() == {'message': 'Hello world!'}
+
+
+def test_read_method():
+    response = client.get('/method')
+    assert response.status_code == 200
+    assert response.json() == {'method': 'GET'}
+
+    response = client.post('/method')
+    assert response.status_code == 201
+    assert response.json() == {'method': 'POST'}
+
+    response = client.delete('/method')
+    assert response.status_code == 200
+    assert response.json() == {'method': 'DELETE'}
+
+    response = client.put('/method')
+    assert response.status_code == 200
+    assert response.json() == {'method': 'PUT'}
+
+    response = client.options('/method')
+    assert response.status_code == 200
+    assert response.json() == {'method': 'OPTIONS'}
+
+
+def test_hash():
+    response = client.get(
+        '/auth?password=kasia&password_hash=77b95508b962ee4320bc31da18776d220b7a42ef0cce3e02a404b049b3d2ac9ddad1d09b4a2928f6dd0942d42e0b87d2bebeb7a78a821e1b57cef29e5b910580')
+    assert response.status_code == 204
+
+    response = client.get(
+        '/auth?password=kasia&password_hash=nu4u3xi7rgcn3gximh23i2crh4bxbdv3u2x')
+    assert response.status_code == 401
+
+    response = client.get(
+        '/auth?password=kasia')
+    assert response.status_code == 401
+
+
+# def test_register():
+#     response = client.post(
+#         '/register', RegisterUser(name='ola', surname='buk'))
+#     assert response.status_code == 201
+#     assert response.json() == {
+#         'id': 1,
+#         'name': 'ola',
+#         'surname': 'buk',
+#         'register_date': datetime.today().strftime('%Y-%m-%d'),
+#         'vaccination_date': (datetime.today() + timedelta(days=6)).strftime('%Y-%m-%d')
+#     }
+
+#     response = client.post(
+#         '/register', RegisterUser(name='arek', surname='marek'))
+#     assert response.status_code == 201
+#     assert response.json() == {
+#         'id': 2,
+#         'name': 'arek',
+#         'surname': 'marek',
+#         'register_date': datetime.today().strftime('%Y-%m-%d'),
+#         'vaccination_date': (datetime.today() + timedelta(days=9)).strftime('%Y-%m-%d')
+#     }
